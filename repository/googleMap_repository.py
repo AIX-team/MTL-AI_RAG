ec06542abd7e69867087b8f62bd3a8b1e2e6ef0c
DESKTOP-OKVQUTP\201-19 <qwes5674@gmail.com>
2025-02-05 18:00:34 +0900

    환경 셋팅

--- repository/googleMap_repository.py
+++ repository/googleMap_repository.py
@@ -1,10 +1,11 @@
-# 임시로 작성한 위치 정보를 데이터 베이스에 저장하는 로직
-from models.googleMap_model import Location
-from typing import List
+import requests
+import os
+from dotenv import load_dotenv
 
-def get_locations() -> List[Location]:
-    return [
-        # Location(name="Location 1", lat=37.4783, lng=126.9512),
-        # Location(name="Location 2", lat=34.0522, lng=-118.2437),
-        # Location(name="Location 3", lat=40.7128, lng=-74.0060),
-    ]
+load_dotenv()
+API_KEY = os.getenv("api_key")
+
+async def get_locations(lat: float, lng: float):
+    url = f"https://maps.googleapis.com/maps/api/geocode/json?latlng={lat},{lng}&key={API_KEY}"
+    response = requests.get(url)
+    return response.json()
--- routers/googleMap.py
+++ routers/googleMap.py
@@ -1,29 +1,8 @@
-from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-from models.googleMap_model import Location
-from repository.googleMap_repository import get_locations
-from typing import List
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
+from fastapi import APIRouter
+from services.googleMap_service import get_location
 
 router = APIRouter()
-templates = Jinja2Templates(directory="templates")
 
-# 지역 
-@router.get("/location", response_model=List[Location])
-def read_location():
-    return [
-         # {"name" : "Location 1", "lat": 37.4783, "lng": 126.9512},
-        # {"name" : "Location 2", "lat": 34.0522, "lng": -118.2437},
-        # {"name" : "Location 3", "lat": 40.7128, "lng": -74.0060 },
-    ]
-
-    
-# api요청
-@router.get("/", response_class=HTMLResponse)
-def read_root(request: Request):
-    api_key = os.getenv("API_KEY")
-    return templates.TemplateResponse("index.html",{"request":request, "api_key":api_key})
+@router.get("/location")
+async def locations(lat: float, lng: float ):
+    return await get_location(lat, lng)
